6b4325f4f08cf85ab5a4d8a68b4cd78fe6448cd5
Clement 'Hyronos' Soler <47113650+clement-soler1@users.noreply.github.com>
2022-03-16 13:41:50 +0100

    maj cards et IA

---
+++ Assets/Scripts/Cards.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cards
+{
+    private string type;
+
+    public Cards(string type)
+    {
+        this.type = type;
+    }
+
+}
--- Assets/Scripts/IA/IA.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IA
-{
-    public Cards drawCard();
-}
