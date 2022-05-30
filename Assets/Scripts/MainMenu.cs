4dfeeb408aeade92f0d81a22e18c72b7bcb350d7
Clement 'Hyronos' Soler <47113650+clement-soler1@users.noreply.github.com>
2022-05-30 23:34:57 +0200

    main menu : landing scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,26 +1,44 @@
 using System.Collections;  
 using System.Collections.Generic;  
 using UnityEngine;  
-using UnityEngine.SceneManagement;  
-public class MainMenu: MonoBehaviour {  
-    
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainMenu: MonoBehaviour {
+
+    public Button play;
+    public Button library;
+    public Button option;
+    public Button credit;
+    public Button leave;
+
+    void Start()
+    {
+        play.onClick.AddListener(PlayGameButton);
+        library.onClick.AddListener(LibraryButton);
+        option.onClick.AddListener(OptionButton);
+        credit.onClick.AddListener(CreditsButton);
+        leave.onClick.AddListener(LeaveGameButton);
+
+    }
+
     //TODO set the Library scene once it's ready
     public void LibraryButton()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+        Debug.Log("Library");
     }
     
     //TODO set the Option scene once it's ready
     public void OptionButton()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+        Debug.Log("Options");
     }
     
     //TODO set the Credits scene once it's ready
     public void CreditsButton()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+        Debug.Log("Crédits");
     }
+
     public void LeaveGameButton()
     {
         Application.Quit();
@@ -29,6 +47,7 @@ public class MainMenu: MonoBehaviour {
     //TODO set the Play scene once it's ready
     public void PlayGameButton()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+        //UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+        Debug.Log("Playing");
     }
 }
